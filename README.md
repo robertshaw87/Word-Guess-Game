9b2304e033e514ae2467b3d437a67d9f1f0800b2
Robert Shaw <robertshaw87@gmail.com>
2018-04-24 00:54:20 -0700

    add initial files, write pseudocode for javascript

---
+++ README.md
@@ -0,0 +1,37 @@
+# Psychic Game
+Game made with BootstrapCDN v4.1.0
+
+Description of Game Here
+
+## Getting Started
+
+Open up the [project link](https://robertshaw87.github.io/Psychic-Game/) and you should be able to see the example webpage shown below:
+
+![Psychic Game](assets/images/readme-game.png "Psychic Game")
+
+INSTRUCTIONS HERE
+
+### Prerequisites
+
+You need to have a modern browser able to handle html5, javascript, and css. 
+
+Some popular browsers are:
+```
+Google Chrome
+Mozilla Firefox
+Safari
+Opera
+Edge
+```
+
+### Installing
+
+No installation needed! Just open the page in your preferred browser.
+
+## Built With
+
+* [BootstrapCDN v4.1.0](https://getbootstrap.com/docs/4.1/getting-started/introduction/) - The web framework used
+
+## Authors
+
+* **Robert Shaw** - *Initial work* - [robertshaw87](https://github.com/robertshaw87)
---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+// declare initial variable values
+// make an array of potential words
+// make an empty array of the letters already guessed by the user
+// decide on a random word to be the target word
+// turn the word into an array of its component letters
+// make an array of place holder characters to represent
+//  correctly guessed letters by the player
+
+// get user guess
+// clean user input
+
+// check user input to make sure it is a new guess
+
+// add the letter to the already guessed array
+
+// if it is a letter within the correct word array,
+// replace the correct placeholder characters in the guess array 
+//  with the letter guessed
+
+// if the guess array is completed and equal to the word array,
+// add 1 to wins, reset guesses, already guessed array,
+//  and guess array to default
+// set a new random word to be guessed and 
+//  format the guess array and word array
+
+// if it is not a correct letter,
+// add guess to already guessed array, reduce guesses left
+
+// if there are no guesses left, increment losses, reset game
+// else go back to getting user input
